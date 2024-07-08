54dcc3bd0d763e6b04f43803a2861bae554eb4db
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2024-07-08 17:14:00 +0530

    updated

--- Drum Kit V1/script.js
+++ Drum Kit V1/script.js
@@ -1,23 +1,60 @@
-"use strict";
+const drumBtns = document.querySelectorAll(".drum");
 
-const keys = document.querySelectorAll(".key");
-
-function playSound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if (!audio) return; // stop function
-  audio.currentTime = "";
-  audio.play();
-  key.classList.add("playing");
+// assigning buttons to the sound
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      let audio1 = new Audio("sounds/tom-4.mp3");
+      audio1.play();
+      break;
+    case "a":
+      const audio2 = new Audio("sounds/tom-3.mp3");
+      audio2.play();
+      break;
+    case "s":
+      const audio3 = new Audio("sounds/tom-2.mp3");
+      audio3.play();
+      break;
+    case "d":
+      const audio4 = new Audio("sounds/tom-1.mp3");
+      audio4.play();
+      break;
+    case "j":
+      const audio5 = new Audio("sounds/snare.mp3");
+      audio5.play();
+      break;
+    case "k":
+      const audio6 = new Audio("sounds/kick-bass.mp3");
+      audio6.play();
+      break;
+    case "l":
+      const audio7 = new Audio("sounds/crash.mp3");
+      audio7.play();
+      break;
+  }
 }
 
-function removeTransition(e) {
-  if (e.propertyName !== "transform") return; // skip if it's not transform
-  this.classList.remove("playing");
+// Animate the button upon clicking
+function btnAnimation(key) {
+  const activeBtn = document.querySelector(`.${key}`);
+  activeBtn.classList.add("playing");
+  setTimeout(() => {
+    activeBtn.classList.remove("playing");
+  }, 100);
 }
 
-window.addEventListener("keydown", playSound);
+// Play sound on button press
+drumBtns.forEach((drum) => {
+  drum.addEventListener("click", (e) => {
+    const btnText = e.target.innerText;
+    makeSound(btnText);
+    btnAnimation(btnText);
+  });
+});
 
-keys.forEach((key) => {
-  key.addEventListener("transitionend", removeTransition);
+// Play sound on key press
+window.addEventListener("keydown", (e) => {
+  const btn = document.querySelector(`.${e.key}`);
+  makeSound(e.key);
+  btnAnimation(e.key);
 });
